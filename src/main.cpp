32675872965cc6d25c6d162c36417142f7c3ca02
Vinit <vinitshaw@hotmail.com>
2016-08-15 18:37:11 -0400

    shaders tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,13 @@
 
 
 #include <iostream>
-#include <math.h>
-#include "../shaders/shaders.h"
 
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
-
 // GLFW
 #include <GLFW/glfw3.h>
-
+#include "../shaders/shaders.h"
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -63,7 +60,7 @@ int main()
     Shader myShader("../shaders/shaders.vs", "../shaders/shaders.frag");
 
     GLfloat vertices1[] = {
-      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Bottom Right
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Bottom Right
     -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Bottom Left
      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Top
     };
@@ -72,18 +69,21 @@ int main()
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
-    //first triangle
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    //position
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+    //color
     glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+
     glBindVertexArray(0);
 
 
-    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    // glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 
     // Game loop
     while (!glfwWindowShouldClose(window))
