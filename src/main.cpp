0a080d835b52aac95c4e4d2f77bee94383fece72
Vinit <vinitshaw@hotmail.com>
2016-09-22 00:41:34 -0400

    EBO

--- src/main.cpp
+++ src/main.cpp
@@ -78,27 +78,38 @@ int main()
     -0.5f, -0.5f, 0.0f,  // Bottom Left
      0.0f,  0.5f, -0.25,    // Top
 
-     0.5f, -0.5f, 0.0f,  // Bottom Right
-     -0.5f, -0.5f, 0.0f,   // Bottom Left //bottom face
+    //  0.5f, -0.5f, 0.0f,  // Bottom Right
+    //  -0.5f, -0.5f, 0.0f,   // Bottom Left //bottom face
      0.0f,  -0.5f, -1.0f, //back
 
-     0.5f, -0.5f, 0.0f,
-     0.0f, -0.5f, -1.0f,
-     0.0f, 0.5f, -0.25f,
-
-     -0.5f, -0.5f, 0.0f,
-     0.0f, -0.5f, -1.0f,
-     0.0f, 0.5f, -0.25
+    //  0.5f, -0.5f, 0.0f,
+    //  0.0f, -0.5f, -1.0f,
+    //  0.0f, 0.5f, -0.25f,
+     //
+    //  -0.5f, -0.5f, 0.0f,
+    //  0.0f, -0.5f, -1.0f,
+    //  0.0f, 0.5f, -0.25
     };
 
-    GLuint VBO, VAO;
+    GLuint indices[] = {
+      0,1,2,
+      0,1,3,
+      0,3,2,
+      1,3,2
+    };
+
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices, GL_STATIC_DRAW);
     //position
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
@@ -145,7 +156,9 @@ int main()
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0,12);
+        // glDrawArrays(GL_TRIANGLES,0,3);
+        // glBindVertexArray(0);
+        glDrawElements(GL_TRIANGLES,12,GL_UNSIGNED_INT,0);
         glBindVertexArray(0);
 
         // Swap the screen buffers
