2b2c88387e1cbd213d7d417cc6c89a4eda9ab391
Vinit <vinitshaw@hotmail.com>
2016-10-03 00:09:03 -0400

    Mesh fix

--- includes/Turnip/mesh.hpp
+++ includes/Turnip/mesh.hpp
@@ -46,7 +46,7 @@ class Mesh{
       glBindBuffer(GL_ARRAY_BUFFER,VBO);
       glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(GLfloat), &vertices.at(0), GL_STATIC_DRAW);
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-      glBufferData(GL_ELEMENT_ARRAY_BUFFER, vertices.size()*sizeof(GLuint), &indices.at(0), GL_STATIC_DRAW);
+      glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(GLuint), &indices.at(0), GL_STATIC_DRAW);
       glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat), (GLvoid*)0);
       glEnableVertexAttribArray(0);
       glBindVertexArray(0);
